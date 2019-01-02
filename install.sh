60edef940480d9570de343836d15f4805e9866c4
Eralp Karaduman <eralpkaraduman@gmail.com>
2019-01-02 15:52:07 +0200

    Added readme

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
-echo "git..."
+printf "git..."
 ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s -f ~/dotfiles/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+echo "ok"
 
-echo "tmux..."
+printf "tmux..."
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+echo "ok"
 
-echo "vim..."
+printf "vim..."
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 rm -rf ~/.vim_runtime
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+git clone --quiet https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "ok"
 
-echo "zsh..."
+printf "zsh..."
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+echo "ok"
 
-echo "vscode..."
+printf "vscode..."
 ln -s -f ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+echo "ok"
 
-echo "done,\nopen a new shell session to reload"
+echo "done.\nopen a new shell session to reload"
