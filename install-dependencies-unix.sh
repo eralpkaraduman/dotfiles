4873616ad2669320ab0d043c95d23ee017c16cc1
Eralp Karaduman <eralp@eralp-noice-mbp.local>
2021-09-13 13:00:18 +0300

    Update

--- install-dependencies-unix.sh
+++ install-dependencies-unix.sh
@@ -3,11 +3,13 @@
 echo "Install nvm https://github.com/nvm-sh/nvm"
 echo "Install node"
 echo "Install zsh (sudo apt install zsh)"
-echo "Install thefuck (https://github.com/nvbn/thefuck#installation)"
+echo "Install tldr"
 echo "Install ag (sudo apt install silversearcher-ag / brew install the_silver_searcher)"
 echo "Install bat (https://github.com/sharkdp/bat#installation)"
 echo "Set alfred preferences folder to folder here"
 echo "Installing vim-plug..."
+
+echo "Set mac keyboard layout to ABC"
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -20,7 +22,6 @@ echo "Installing antigen..."
 echo "Installing fzf..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
-#echo "Installing thefuck"
 #npm install --global pure-prompt
 
 echo "Install pure (prompt) (try starship maybe?)"
