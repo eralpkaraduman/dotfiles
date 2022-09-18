b0b1b73202fab6bf715ceda664199158f7688443
Eralp Karaduman <eralp@eralpkaraduman.com>
2022-09-18 15:14:19 +0300

    Update

--- push.sh
+++ push.sh
@@ -26,7 +26,7 @@ echo "Updating obsidian config"
 cp ~/Documents/Obsidian\ Vault/obsidian.vimrc .
 
 echo "Pushing to repository"
-git diff # test
+git diff --quiet # test
 git add .
 git clean -df
 git commit -m "Update"
